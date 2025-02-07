9bbaee91d501db3d0d5e9de25f09b955a0b253a9
6Vikich9 <v.hnetsko@gmail.com>
2025-02-08 01:28:38 +0200

    add first hw

---
+++ HW_1/1/main.js
@@ -0,0 +1,51 @@
+/*#dYQNrBV
+Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+Вивести кожну змінну за допомогою: console.log*/
+
+let greeting;
+greeting = 'hello';
+console.log(greeting);
+
+let owu;
+okten = 'owu';
+console.log(okten);
+
+let com;
+com = 'com';
+console.log(com);
+
+let number1;
+number1 = 1;
+console.log(number1);
+
+let number2;
+number2 = 10;
+console.log(number2);
+
+let number3;
+number3 = -999;
+console.log(number3);
+
+let number4;
+number4 = 123;
+console.log(number4);
+
+let number5;
+number5 = 3.14;
+console.log(number5);
+
+let number6;
+number6 = 2.7;
+console.log(number6);
+
+let number7;
+number7 = 16;
+console.log(number7);
+
+let boolt;
+boolt = true;
+console.log(boolt);
+
+let boolf;
+boolf = false;
+console.log(boolf);
---
+++ HW_1/4/main.js
@@ -0,0 +1,6 @@
+/*- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль*/
+
+let name= prompt('Enter your name', '');
+let lastName= prompt('Enter your last name', '');
+let birthday= prompt('Enter your birthday year', '');
+alert(name + ' ' + lastName + ' ' + birthday);
